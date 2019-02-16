5efe74b3cd66a9b8284a17c226a37f819f442c32
MiltonLn <miltonln04@gmail.com>
2019-02-16 11:02:57 -0500

    Updated Contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 import { SocialMedia } from './social-media'
-import { data } from '../data'
 
 
 export class Contact extends Component {
@@ -11,27 +11,13 @@ export class Contact extends Component {
         <h2 className="section-title">Get in Touch</h2>
 
         <div className="intro">
-          <img className="profile-image" src={ data.picture } alt=""/>
+          <img className="profile-image" src={ this.props.data.picture } alt=""/>
           <div className="dialog">
-            <p>
-              If you want to discuss about Programming, Open Source, Programming communities, Soccer or Videogames
-              I'm the one you are looking for, also, I can take some freelance work.
-            </p>
-            <p><strong>I can help with the following:</strong></p>
-            <ul className="list-unstyled service-list">
-              <li><i className="fa fa-check" aria-hidden="true"></i> Front-end development with ReactJS</li>
-              <li><i className="fa fa-check" aria-hidden="true"></i> Back-end development with Django/Python</li>
-              <li><i className="fa fa-check" aria-hidden="true"></i> API crafting and maintenance with Django Rest Framework and GraphQL</li>
-              <li><i className="fa fa-check" aria-hidden="true"></i> Content oriented sites building with Wagtail</li>
-              <li><i className="fa fa-check" aria-hidden="true"></i> UI development</li>
-              <li><i className="fa fa-check" aria-hidden="true"></i> Testing and CI processes implementation</li>
-            </ul>
-            <p>
-              Drop me a line at <a href={`mailto:${ data.email }`}>{ data.email }</a>&nbsp;
-              or call me at <a href={`tel:${ data.phone }`}>{ data.phone }</a>
-            </p>
+            <ReactMarkdown
+              source={this.props.data.contactContent}
+              escapeHtml={false}/>
 
-            <SocialMedia/>
+            <SocialMedia data={this.props.data}/>
           </div>
         </div>
 
